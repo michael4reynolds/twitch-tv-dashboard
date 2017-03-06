00317ff903568afd1240e9cb99e4c27b60f77b43
Michael Reynolds <michael4reynolds@gmail.com>
2017-03-06 17:40:31 -0500

    User story: can see if fcc is currently streaming

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ _See who's online and broadcasting right now!_
 
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
+### Client ID:
+add your twitch tv api client id to .env file as REACT_APP_ClIENT_ID=yourTwitchApiClientID
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,30 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 // Model
+const twitchApi = 'https://api.twitch.tv/kraken'
+const streams = `${twitchApi}/streams`
+// const channels = `${twitchApi}/channels`
+// const twitchLink = 'https://www.twitch.tv/'
+const headers = {'client-id': process.env.REACT_APP_ClIENT_ID}
+
+const queryParams = (channel) => ({channel})
 
 // View
+const isOnline = document.querySelector('p span')
 
 // Controller
+const getIsOnline = async (channels) => {
+  const {data} = await axios.get(streams, {
+    headers: headers,
+    params: queryParams(channels)
+  })
+  return !!+data._total
+}
 
 // initialize
 const init = () => {
   try {
-    console.log('begin')
+    getIsOnline('freecodecamp').then(online => isOnline.innerText = `online: ${online}`)
   } catch (e) {
     console.log(e)
   }
