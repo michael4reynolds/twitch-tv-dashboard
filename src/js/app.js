1ee532d975155e0a5cdd1dda53f663e75b0f1b40
Michael Reynolds <michael4reynolds@gmail.com>
2017-03-06 20:27:46 -0500

    User story: click status output and sent to channel

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,14 @@ import axios from 'axios'
 const twitchApi = 'https://api.twitch.tv/kraken'
 const streams = `${twitchApi}/streams`
 // const channels = `${twitchApi}/channels`
-// const twitchLink = 'https://www.twitch.tv/'
+const twitchLink = 'https://www.twitch.tv'
 const headers = {'client-id': process.env.REACT_APP_ClIENT_ID}
 
 const queryParams = (channel) => ({channel})
 
 // View
 const isOnline = document.querySelector('p span')
+const link = document.querySelector('a')
 
 // Controller
 const getIsOnline = async (channels) => {
@@ -24,7 +25,11 @@ const getIsOnline = async (channels) => {
 // initialize
 const init = () => {
   try {
-    getIsOnline('freecodecamp').then(online => isOnline.innerText = `online: ${online}`)
+    let channels = 'freecodecamp'
+    getIsOnline(channels).then(online => {
+      link.href = `${twitchLink}/${channels}`
+      isOnline.innerText = `online: ${online}`
+    })
   } catch (e) {
     console.log(e)
   }
